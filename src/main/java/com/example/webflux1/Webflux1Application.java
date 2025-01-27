57cf745f91efc97527c64750549c7d04f6f7f3d3
yujinlee <now8567@hanmail.net>
2025-01-27 17:28:09 +0900

    blockhound 추가

--- src/main/java/com/example/webflux1/Webflux1Application.java
+++ src/main/java/com/example/webflux1/Webflux1Application.java
@@ -1,13 +1,34 @@
 package com.example.webflux1;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
 
 @SpringBootApplication
-public class Webflux1Application {
+public class Webflux1Application implements ApplicationRunner {
 
 	public static void main(String[] args) {
+
+		BlockHound.install();
 		SpringApplication.run(Webflux1Application.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		// blockhound 동작 테스트
+	/*	Mono.delay(Duration.ofSeconds(1))
+				.doOnNext(it -> {
+                    try {
+                        Thread.sleep(100); // blocking
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+				.subscribe();*/
+	}
 }
--- src/test/java/com/example/webflux1/controller/UserControllerTest.java
+++ src/test/java/com/example/webflux1/controller/UserControllerTest.java
@@ -3,16 +3,23 @@ package com.example.webflux1.controller;
 import com.example.webflux1.dto.UserCreateRequest;
 import com.example.webflux1.dto.UserResponse;
 import com.example.webflux1.repository.User;
+import com.example.webflux1.service.PostService;
+import com.example.webflux1.service.PostServieV2;
 import com.example.webflux1.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cglib.core.Block;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.blockhound.BlockHound;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,11 +29,33 @@ import static org.mockito.Mockito.when;
 @AutoConfigureWebTestClient
 class UserControllerTest {
 
+    static {
+        BlockHound.install(
+                builder -> builder.allowBlockingCallsInside("reactor.test.MessageFormatter", "fail")
+        );
+    }
+
     @Autowired
     private WebTestClient webTestClient;
 
     @MockBean
     private UserService userService;
+
+    @MockBean
+    private PostServieV2 postService;
+
+    @Test
+    void blockHoundTest(){
+        StepVerifier.create(Mono.delay(Duration.ofSeconds(1))
+                .doOnNext(it -> {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+        ).verifyComplete();
+    }
 
     @Test
     void createUser() {
